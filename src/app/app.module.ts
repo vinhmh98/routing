4ebb49571612ac7e43947ca9f363a94b9f5733ea
vinhmh98 <56551461+vinhmh98@users.noreply.github.com>
2020-08-10 07:10:44 +0700

    vinhpro1298

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AboutComponent } from './component/about/about.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { HomeComponent } from './component/home/home.component';
+import { NotFoundComponent } from './component/not-found/not-found.component';
 
+const appRouter: Routes = [
+  {
+    path : '',
+    component : HomeComponent
+  },
+  {path : 'about',
+  component: AboutComponent},
+  {path : 'contact',
+  component: ContactComponent},
+  {
+    path : '**',
+    component: NotFoundComponent
+  }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRouter),
   ],
   providers: [],
   bootstrap: [AppComponent]
